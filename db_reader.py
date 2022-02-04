11b584525bd4789c7c43dabe6d796e565cad3d7e
andreykbjj <andrey.k.bjj@gmail.com>
2022-02-04 13:18:44 +0200

    test-10

--- db_reader.py
+++ db_reader.py
@@ -5,5 +5,6 @@ cursor = connection.cursor()
 sqlite_select_query = """SELECT * from BITTREX"""
 cursor.execute(sqlite_select_query)
 records = cursor.fetchall()
-print(*records)
+for line in records:
+    print(line)
 connection.close()
