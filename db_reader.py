7a346ce654b9714099204207b720b741bc8f6c88
andreykbjj <andrey.k.bjj@gmail.com>
2022-02-04 13:13:17 +0200

    test-9

---
+++ db_reader.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+connection = sqlite3.connect('BITTREX_TICKER.sqlite')
+cursor = connection.cursor()
+sqlite_select_query = """SELECT * from BITTREX"""
+cursor.execute(sqlite_select_query)
+records = cursor.fetchall()
+print(*records)
+connection.close()
